ab1f1ea6065466af9610dd3e71b8e46b7872417c
Marta Manchur <mmmz1004@gmail.com>
2019-02-03 21:40:10 +0200

    added scroll up-down effect

---
+++ Landing/Landing/Pages/Mobile/Step.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Landing.Pages.Mobile
+{
+    public class StepModel : PageModel
+    {
+        public string ImageSource { get; set; }
+
+        public string Header { get; set; }
+
+        public string TextContent { get; set; }
+
+        public void OnGet()
+        {
+
+        }
+    }
+}
