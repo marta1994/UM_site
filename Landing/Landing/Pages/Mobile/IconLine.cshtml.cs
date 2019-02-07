81290f1678374c400c425b16380d0756fe339981
Marta Manchur <mmmz1004@gmail.com>
2019-02-07 22:43:34 +0200

    changed to svg

--- Landing/Landing/Pages/Mobile/IconLine.cshtml.cs
+++ Landing/Landing/Pages/Mobile/IconLine.cshtml.cs
@@ -10,25 +10,31 @@ namespace Landing.Pages.Mobile
             public string Icon { get; set; }
 
             public string Link { get; set; }
+
+            public string Class { get; set; }
         }
 
         public List<IconItem> IconItems = new List<IconItem>
         {
             new IconItem {
                 Link = "https://www.facebook.com/UstymManchur/",
-                Icon = "/images/facebook_icon.png"
+                Icon = "/images/facebook_icon.svg",
+                Class = "facebook-link"
             },
             new IconItem {
                 Link = "viber://chat?number=+380631732306",
-                Icon = "/images/viber_icon.jpg"
+                Icon = "/images/viber_icon.svg",
+                Class = "viber-link"
             },
             new IconItem {
                 Link = "https://www.pinterest.com/ustym1996/",
-                Icon = "/images/pinterest_icon.png"
+                Icon = "/images/pinterest_icon.svg",
+                Class = "pinterest-link"
             },
             new IconItem {
                 Link = "https://www.instagram.com/y.m_jewelry/",
-                Icon = "/images/instagram_icon.png"
+                Icon = "/images/instagram_icon.svg",
+                Class = "instagram-link"
             }
         };
 
